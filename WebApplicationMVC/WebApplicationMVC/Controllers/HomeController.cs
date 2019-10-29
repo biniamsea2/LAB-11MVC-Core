c5846cd65d29ba9d80c1ed250ddc89b22eeab43d
Biniam <biniamt23@yahoo.com>
2019-10-29 10:51:42 -0700

    worked on master forn first part of this lab

---
+++ WebApplicationMVC/WebApplicationMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int year, string honor, string name, string country, int birthYear, int deathYear, string title, string category, string context)
+        {
+            TimePerson person = new TimePerson(year,  honor,  name,  country,  birthYear,  deathYear,  title,  category,  context);
+            return RedirectToAction("Results", person);
+        }
+
+        public ViewResult Results(TimePerson person)
+        {
+            return View(person);
+        }
+
+
+        public string Test(string name, int age)
+        {
+            return $"Hello {name}, you're {age} years old";
+        }
+    }
+}
---
+++ WebApplicationMVC/WebApplicationMVC/Models/TimePerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC.Models
+{
+    public class TimePerson
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        public TimePerson(int year, string honor, string name, string country, int birthYear, int deathYear, string title, string category, string context)
+        {
+            Year = year;
+            Honor = honor;
+            Name = name;
+            Country = country;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            Title = title;
+            Category = category;
+            Context = context;
+        }
+    }
+}
